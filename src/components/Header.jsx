f8b2b333e793037b6f3184e90c9663c7dcfb618b
KRISHGOKULLLLLL <b.krishgokulmes17@gmail.com>
2026-09-18 06:11:16 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>🎓 Student Dashboard</h1>
+      <p>My First React Project</p>
+    </header>
+  );
+}
+
+export default Header;
